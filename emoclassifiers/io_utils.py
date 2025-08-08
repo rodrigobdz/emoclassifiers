b4f61ef150d8d27ea5f11eabadc81655f44eb818
Rodrigo Bermudez Schettino <rodrigobdz@users.noreply.github.com>
2025-08-08 14:53:23 +0200

    Modify get_path to find files in fork

--- emoclassifiers/io_utils.py
+++ emoclassifiers/io_utils.py
@@ -1,12 +1,12 @@
 import json
-import os
+from importlib import resources
 
 
 def load_json(path: str) -> dict:
     """
     Load a JSON file.
     """
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return json.load(f)
 
 
@@ -14,7 +14,7 @@ def save_json(data: dict, path: str):
     """
     Save a JSON file.
     """
-    with open(path, 'w') as f:
+    with open(path, "w") as f:
         json.dump(data, f)
 
 
@@ -22,7 +22,7 @@ def load_jsonl(path: str) -> list[dict]:
     """
     Load a JSONL file.
     """
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return [json.loads(line) for line in f]
 
 
@@ -30,14 +30,10 @@ def save_jsonl(data: list[dict], path: str):
     """
     Save a JSONL file.
     """
-    with open(path, 'w') as f:
+    with open(path, "w") as f:
         for item in data:
-            f.write(json.dumps(item) + '\n')
+            f.write(json.dumps(item) + "\n")
 
 
 def get_path(rel_path: str) -> str:
-    """
-    Get the path to a file in the project.
-    """
-    base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(base_path, rel_path)
+    return str(resources.files("emoclassifiers").joinpath(rel_path))
